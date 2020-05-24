a47ed642b4c8df02a6fda09c0c9b2a1731b153d5
Rodrigo Ledesma Pinet <50053346+roipinet@users.noreply.github.com>
2020-05-24 18:59:46 -0500

    Create greenScreen.js

---
+++ greenScreen.js
@@ -0,0 +1,26 @@
+// Enrique Rodrigo Ledesma Pinet
+// Duke LTP Week 2
+// Green Screen algorithm
+
+// Foreground image:
+var fgImage = new SimpleImage("drewRobert.png");
+// Background image:
+var bgImage = new SimpleImage("dinos.png");
+//Output image:
+var output = new SimpleImage(fgImage.getWidth(), fgImage.getHeight());
+
+//Go through all pixels in the foreground
+for (var pixel of fgImage.values()){
+    // Check if the current pixel goes above the threshold
+    if(pixel.getGreen() > pixel.getBlue() + pixel.getRed()){
+        //get the equivalent pixel in the background
+        var bgX = pixel.getX();
+        var bgY = pixel.getY();
+        var bgPixel = bgImage.getPixel(bgX,bgY);
+        //Paste this pixel in the output image
+        output.setPixel(bgX, bgY, bgPixel);
+    }
+    //IF it does not go above the threshold, paste the foreground verision
+    else{
+        output.setPixel(pixel.getX(),pixel.getY(), pixel);
+    }
