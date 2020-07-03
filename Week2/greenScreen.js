5f5cef72d50ba6aaf24e06eb367f008dc57f2f50
Rodrigo Ledesma Pinet <50053346+roipinet@users.noreply.github.com>
2020-07-03 18:42:17 -0500

    Update and rename greenScreen.js to Week2/greenScreen.js

--- Week2/greenScreen.js
+++ Week2/greenScreen.js
@@ -1,6 +1,25 @@
-// Enrique Rodrigo Ledesma Pinet
-// Duke LTP Week 2
-// Green Screen algorithm
+// Author: Enrique Rodrigo Ledesma Pinet | email: rodrigopinet@outlook.com
+// Duke LTP Course 1 Week 2
+// Greenscreen algorithm
+
+//Function definition
+function swapBlueYellow(pixel){
+    pixel.setRed(255);
+    pixel.setGreen(255);
+    pixel.setBlue(0);
+    return pixel;
+}
+
+//Open image
+var img = new SimpleImage("duke_blue_devil.png");
+//iterate through all pixels
+for(var pixel of img.values()){
+    if(pixel.getRed() == 0 && pixel.getBlue() > 200 && pixel.getGreen() > 40 && pixel.getGreen() < 60){
+    img.setPixel(pixel.getX(), pixel.getY(), swapBlueYellow(pixel));
+    }
+}
+//Show image
+print(img);
 
 // Foreground image:
 var fgImage = new SimpleImage("drewRobert.png");
